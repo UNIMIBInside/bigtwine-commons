ced492ecb5df378e8918ca2afd937a74ce1591c1
Fausto <fausto91@gmail.com>
2019-01-08 21:51:31 +0100

    Refactoring sync/async executors

--- src/main/java/it/unimib/disco/bigtwine/commons/executors/AsyncExecutor.java
+++ src/main/java/it/unimib/disco/bigtwine/commons/executors/AsyncExecutor.java
@@ -2,5 +2,4 @@ package it.unimib.disco.bigtwine.commons.executors;
 
 public interface AsyncExecutor extends Executor {
     void setListener(AsyncExecutorListener listener);
-    void execute(Object... args);
 }
--- src/main/java/it/unimib/disco/bigtwine/commons/executors/SyncExecutor.java
+++ src/main/java/it/unimib/disco/bigtwine/commons/executors/SyncExecutor.java
@@ -1,5 +1,4 @@
 package it.unimib.disco.bigtwine.commons.executors;
 
 public interface SyncExecutor extends Executor {
-    String execute(Object... args);
 }
