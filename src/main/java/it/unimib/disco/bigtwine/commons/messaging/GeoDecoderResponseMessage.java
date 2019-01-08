367440bc9b570a4e9d062da6d2419b5b612efe41
Fausto <fausto91@gmail.com>
2019-01-08 22:58:38 +0100

    Implementazione GeoDecoder messaging

--- src/main/java/it/unimib/disco/bigtwine/commons/messaging/GeoDecoderResponseMessage.java
+++ src/main/java/it/unimib/disco/bigtwine/commons/messaging/GeoDecoderResponseMessage.java
@@ -6,4 +6,28 @@ public class GeoDecoderResponseMessage {
     private String requestId;
     private String decoder = "default";
     private Address[] addresses;
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public String getDecoder() {
+        return decoder;
+    }
+
+    public void setDecoder(String decoder) {
+        this.decoder = decoder;
+    }
+
+    public Address[] getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(Address[] addresses) {
+        this.addresses = addresses;
+    }
 }
