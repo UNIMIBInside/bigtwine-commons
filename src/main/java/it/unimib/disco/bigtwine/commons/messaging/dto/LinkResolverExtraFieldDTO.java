9dd96316185b85e9a0a91e69a019071dac94c9b4
Fausto <fausto91@gmail.com>
2019-11-01 09:49:50 +0100

    Fix serializzazione LinkResolverExtraFieldDTO

--- src/main/java/it/unimib/disco/bigtwine/commons/messaging/dto/LinkResolverExtraFieldDTO.java
+++ src/main/java/it/unimib/disco/bigtwine/commons/messaging/dto/LinkResolverExtraFieldDTO.java
@@ -1,10 +1,19 @@
 package it.unimib.disco.bigtwine.commons.messaging.dto;
 
-public class LinkResolverExtraFieldDTO {
+import java.io.Serializable;
+
+public class LinkResolverExtraFieldDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String valuePath;
     private String saveAs;
 
     public LinkResolverExtraFieldDTO() {
+    }
+
+    public LinkResolverExtraFieldDTO(String valuePath, String saveAs) {
+        this.valuePath = valuePath;
+        this.saveAs = saveAs;
     }
 
     public String getValuePath() {
